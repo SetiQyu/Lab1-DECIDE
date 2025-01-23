480ccf6b14a8748d3b4d9a87e504fedf8ce82ffb
Adam Rasmussen <ajfr2@kth.se>
2025-01-23 13:00:11 +0100

    Fix general styling and errors in previous skeleton
    Moved some of the comments to reduce the length of the skeleton.
    Some variables were not assigned a value when creating a parameter instance.

--- app/src/main/java/DECIDE/Decide.java
+++ app/src/main/java/DECIDE/Decide.java
@@ -1,54 +1,39 @@
 public class Decide {
     // Type definitions 
     public class Parameters {
-        // LENGTH1: Length in LICs 0, 7, 12
-        // RADIUS1: Radius in LICs 1, 8, 13
-        // EPSILON: Deviation from PI in LICs 2, 9
-        // AREA1: Area in Lics 3, 10, 14
-        // Q_PTS: No. of consecutive points in LIC 4
-        // QUADS: No. of quadrants in LIC 4
-        // DIST: Distance in LIC 6
-        // N_PTS: No. of int. pts. in LICs 7, 12
-        // K_PTS: No. of int. pts. in LICs 8, 13
-        // B_PTS: No. of int. pts. in LICs 8, 13
-        // C_PTS: No. of int. pts. in LIC 9
-        // D_PTS: No. of int. pts. in LIC 9
-        // E_PTS: No. of int. pts. in LICs 10, 14
-        // F_PTS: No. of int. pts. in LICs 10, 14
-        // G_PTS: No. of int. pts. in LIC 11
-        // LENGTH2: Maximum length in LIC 12
-        // RADIUS2: Maximum radius in LIC 13
-        // AREA2:  Maximum area in LIC 14
-        double LENGTH1, RADIUS1, EPSILON, AREA1, DIST, LENGTH2, RADIUS2, AREA2;
-        int Q_PTS, QUADS, N_PTS, K_PTS, A_PTS, B_PTS, C_PTS, D_PTS, E_PTS, F_PTS, G_PTS;
+        public double LENGTH1, RADIUS1, EPSILON, AREA1, DIST, LENGTH2, RADIUS2, AREA2;
+        public int Q_PTS, QUADS, N_PTS, K_PTS, A_PTS, B_PTS, C_PTS, D_PTS, E_PTS, F_PTS, G_PTS;
         
+        // Creates a parameter instance from the lists p1 and p2. 
         public Parameters(double[] p1, int[] p2) {
-            // Double typed variables
-            this.LENGTH1 = p1[0];
-            this.RADIUS1 = p1[1];
-            this.EPSILON = p1[2];
-            this.AREA1 = p1[3];
-            this.DIST = p1[4];
-            this.LENGTH2 = p1[5];
-            this.RADIUS2 = p1[6];
+            // Double-type variables
+            this.LENGTH1 = p1[0]; // LENGTH1: Length in LICs 0, 7, 12
+            this.RADIUS1 = p1[1]; // RADIUS1: Radius in LICs 1, 8, 13
+            this.EPSILON = p1[2]; // EPSILON: Deviation from PI in LICs 2, 9
+            this.AREA1 = p1[3]; // AREA1: Area in Lics 3, 10, 14
+            this.DIST = p1[4]; // DIST: Distance in LIC 6
+            this.LENGTH2 = p1[5]; // LENGTH2: Maximum length in LIC 12
+            this.RADIUS2 = p1[6]; // RADIUS2: Maximum radius in LIC 13
+            this.AREA2 = p1[7]; // AREA2:  Maximum area in LIC 14
 
-            // Int type variables
-            this.Q_PTS = p2[0];
-            this.QUADS = p2[1];
-            this.N_PTS = p2[2];
-            this.K_PTS = p2[3];
-            this.A_PTS = p2[4];
-            this.B_PTS = p2[5];
-            this.C_PTS = p2[6];
-            this.D_PTS = p2[7];
-            this.E_PTS = p2[8];
-            this.G_PTS = p2[9];
+            // Int-type variables
+            this.Q_PTS = p2[0]; // Q_PTS: No. of consecutive points in LIC 4
+            this.QUADS = p2[1]; // QUADS: No. of quadrants in LIC 4
+            this.N_PTS = p2[2]; // N_PTS: No. of int. pts. in LICs 7, 12
+            this.K_PTS = p2[3]; // K_PTS: No. of int. pts. in LICs 8, 13
+            this.A_PTS = p2[4]; // A_PTS: No. of int. pts. in LICs 8, 13
+            this.B_PTS = p2[5]; // B_PTS: No. of int. pts. in LICs 8, 13
+            this.C_PTS = p2[6]; // C_PTS: No. of int. pts. in LIC 9
+            this.D_PTS = p2[7]; // D_PTS: No. of int. pts. in LIC 9
+            this.E_PTS = p2[8]; // E_PTS: No. of int. pts. in LICs 10, 14
+            this.F_PTS = p2[9]; // F_PTS: No. of int. pts. in LICs 10, 14
+            this.G_PTS = p2[10]; // G_PTS: No. of int. pts. in LIC 11
         }
     }
-    public enum CONNECTORS { 
+    public static enum CONNECTORS { 
         NOTUSED(777),
-        ORR(778),
-        ANDD(779);
+        ORR(778), // 778 was not specified in the assignment.
+        ANDD(779); // 779 was not specified in the assignment
     
         private final int val;
         CONNECTORS(int value){
@@ -62,9 +47,9 @@ public class Decide {
 
     
     public static enum COMPTYPE {
-        LT(1111),
-        EQ(1110),
-        GT(1100);
+        LT(1111), 
+        EQ(1110), // 1110 was not specified in the assignment
+        GT(1100); // 1100 was not specified in the assignment
 
         private final int compVal;
 
@@ -73,7 +58,7 @@ public class Decide {
                 this.compVal = value;
             }
             else{
-                throw new IllegalArgumentException("Argument \"value\" must be 1111, 1110 or 1100)");
+                throw new IllegalArgumentException("Argument \"value\" must be 1111(LT), 1110(EQ) or 1100(GT))");
             }
         }
 
