c0369ee8b6206b50b86f8115bb62214bbf441405
Adam <adamfriden00@gmail.com>
2025-01-26 21:20:21 +0100

    Feature: #28 Implement Lic12
    Adds the required functionality for Lic12 and 3 corresponding tests.

---
+++ app/src/main/java/app/decide/lic/Lic12.java
@@ -0,0 +1,39 @@
+package app.decide.lic;
+import app.decide.Decide.*;
+
+public class Lic12 implements ILic{
+    @Override
+    public boolean condition(double[] x, double[] y, int num_points, Parameters parameters){
+        // Pre-conditions
+        if(num_points < 3) return false;
+        if(parameters.LENGTH2 < 0) return false;
+        if((x.length != y.length) || (num_points != y.length)){
+            throw new IllegalArgumentException("The length of the inputs do not match");
+        } 
+        
+        // Local variables
+        int compare_index;
+        int max_index = num_points - parameters.K_PTS - 2;
+        boolean l1_found = false;
+        boolean l2_found = false;
+        // Check if condition for LIC12 is met
+        for(int i = 0; i <= max_index; i++){
+            compare_index = i + parameters.K_PTS + 1;
+            if(euclidian_distance(new double[] {x[i], y[i]}, new double[] {x[compare_index], y[compare_index]}) > parameters.LENGTH1){
+                l1_found = true;
+            }
+            if(euclidian_distance(new double[] {x[i], y[i]}, new double[] {x[compare_index], y[compare_index]}) < parameters.LENGTH2){
+                l2_found = true; 
+            }
+            if(l1_found && l2_found) return true;
+        }
+        return false; 
+    }
+
+    public double euclidian_distance(double[] p, double[] q){
+        return Math.sqrt(
+            Math.pow((p[0] - q[0]), 2) +
+            Math.pow((p[1] - q[1]), 2)
+        );
+    }
+}
