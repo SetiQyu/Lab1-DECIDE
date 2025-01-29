5630155769542d224b40df98f6ba0533fb7cb67d
TheCoder <thecoder@TheCoder>
2025-01-29 12:46:52 +0100

    feature: #49 Implement decide logic

--- app/src/test/java/app/decide/DecideTest.java
+++ app/src/test/java/app/decide/DecideTest.java
@@ -5,8 +5,5 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 class DecideTest {
-    @Test void testGradleTesting() {
-        Decide classUnderTest = new Decide();
-        assertEquals(classUnderTest.writeOut(), "Test print", "Testing tests");
-    }
+    
 }
