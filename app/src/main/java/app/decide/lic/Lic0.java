a9fcd8781a420563f6650099a5789f8d4b3cc902
samer jameel <samer.bassam.01@gmail.com>
2025-01-29 21:44:17 +0100

    initial commit

--- app/src/main/java/app/decide/lic/Lic0.java
+++ app/src/main/java/app/decide/lic/Lic0.java
@@ -1,12 +1,17 @@
 package app.decide.lic;
 import app.decide.Decide;
-import java.lang.Math; 
 
 public class Lic0 implements ILic{
-    public boolean distance_points(double[] x, double[] y, int num_points, Decide.Parameters parameters, double length1) {
+
+    @Override
+    public boolean condition(double[] x, double[] y, int num_points, Decide.Parameters parameters) {
+        return distance_points(x, y, num_points, parameters);
+    }
+
+    public boolean distance_points(double[] x, double[] y, int num_points, Decide.Parameters parameters) {
         for (int i = 0; i < num_points-1; i++) {
             double distance = Math.sqrt( Math.pow(x[i+1]- x[i], 2) + Math.pow(y[i+1]- y[i], 2) );
-            if(distance > length1) {
+            if(distance > parameters.LENGTH1) {
                 return true;
             }
         }
