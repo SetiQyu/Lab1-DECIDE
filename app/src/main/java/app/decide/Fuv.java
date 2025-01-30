1b98212acae528d327df8a928acc9dfa44c4edd7
Leo <leo.lundberg99@gmail.com>
2025-01-30 15:01:38 +0100

    Fix: #42 Add error-handling and tests for FUV

--- app/src/main/java/app/decide/Fuv.java
+++ app/src/main/java/app/decide/Fuv.java
@@ -10,8 +10,25 @@ public class Fuv {
     * @param dimension an integer that should be 15
     * @return FUV 15 element long boolean array
     */
+    public static boolean catchBadPUMLength(boolean[][] PUM, int dimension){
+        
+        for(boolean[] row : PUM){
+            if(row.length != dimension){
+                return true;
+            }
+        }
+        return false;
+    }
     public static boolean[] getFUV(boolean[][] PUM, boolean[] PUV, int dimension){
         
+        if(PUV.length != dimension){
+            throw new IllegalArgumentException("Length of PUV did not match supplied dimension parameter");
+        }
+        
+        if(PUM.length != dimension || catchBadPUMLength(PUM, dimension)){
+            throw new IllegalArgumentException("PUM dimensions did not match supplied dimension parameter");
+        }
+
         boolean[] FUV = new boolean[dimension];
         
         for(int i = 0; i < dimension; i++){
